434a56ee35a8169d627cb5efa8149b0943477ee7
kyriejoshua <withoutIverson@outlook.com>
2021-04-14 13:54:45 +0800

    :memo: 修正逻辑

--- Sample/Ch06/PRACTICE.sql
+++ Sample/Ch06/PRACTICE.sql
@@ -16,7 +16,7 @@ SELECT sale_price
 
 -- 正确的✅
 SELECT
-  SUM(CASE WHEN sale_price <= 1000 THEN 1 ELSE 0 END) AS low_price
-  SUM(CASE WHEN sale_price BETWEEN(1001 AND 3000) THEN 1 ELSE 0 END) AS mid_price
-  SUM(CASE WHEN sale_price >= 3001 THEN 1 ELSE 0 END) AS high_price
+  SUM(CASE WHEN sale_price <= 1000 THEN 1 ELSE 0 END) AS low_price,
+  SUM(CASE WHEN sale_price BETWEEN(1001 AND 3000) THEN 1 ELSE 0 END) AS mid_price,
+  SUM(CASE WHEN sale_price >= 3001 THEN 1 ELSE 0 END) AS high_price,
   FROM Product
